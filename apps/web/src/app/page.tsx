23fd5169031e522aa25fd3ed4966aa7f61408140
Egor <egor.corostin@gmail.com>
2023-11-03 10:55:50 +0400

    init next14

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import {ThemeSwitcher} from "@/components/app/core/ThemeSwitcher";
+
+export default function Home() {
+  return (
+    <>
+      <ThemeSwitcher />
+    </>
+  )
+}
