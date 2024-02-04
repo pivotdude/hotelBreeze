4a931103492be44685dc9b9c3684b7361ff896f0
Egor <egor.corostin@gmail.com>
2024-02-05 00:00:13 +0400

    added path

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -1,7 +1,7 @@
 type TSwitch = 'enabled' | 'disabled'
 
 const constants = {
-  BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL as string,
+  BACKEND_URL: (process.env.NEXT_PUBLIC_BACKEND_URL as string) || 'https://hotelbreeze.vercel.app/',
   STABS_ENABLE: process.env.NEXT_PUBLIC_MOCK_APIS as TSwitch,
   NODE_MODE: process.env.NODE_ENV,
 }
