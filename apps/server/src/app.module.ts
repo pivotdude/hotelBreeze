a24d92e526e524e565ecd0c2b07ce7155d961dc1
Egor <egor.corostin@gmail.com>
2024-02-04 19:05:24 +0400

    update auto schema

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -39,7 +39,7 @@ import { RootResolver } from './root.resolver'
     ReviewModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true, //join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: process.env.NODE_ENV !== 'production' ? join(process.cwd(), 'src/schema.gql') : false,
       // typeDefs: [join(process.cwd(), 'src/schema.gql')],
       installSubscriptionHandlers: true,
       playground: false,
