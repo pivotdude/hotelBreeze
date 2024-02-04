6cd5a5dc60f9a10947efecc33442a308f5e390f0
Egor <egor.corostin@gmail.com>
2024-02-04 23:52:34 +0400

    deleted path

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -1,7 +1,7 @@
 type TSwitch = 'enabled' | 'disabled'
 
 const constants = {
-  BACKEND_URL: (process.env.NEXT_PUBLIC_BACKEND_URL as string) || 'https://hotel-breeze-server.vercel.app',
+  BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL as string,
   STABS_ENABLE: process.env.NEXT_PUBLIC_MOCK_APIS as TSwitch,
   NODE_MODE: process.env.NODE_ENV,
 }
