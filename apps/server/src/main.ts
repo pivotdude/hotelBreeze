447faf5ab0d15509cd5983ecc5cb870dd731e1c2
Egor <egor.corostin@gmail.com>
2024-02-05 19:53:43 +0400

    listen all ips

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
   })
 
-  await app.listen(3005)
+  await app.listen(3005, '0.0.0.0')
   console.log(`Application is running on: ${await app.getUrl()}`)
   console.log(`GraphQL Playground: ${await app.getUrl()}/graphql`)
 }
